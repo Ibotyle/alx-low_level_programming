6d68253e4ed86bbdc1362cbdc63a8fc90e264619
Awajis <ibotyle@gmail.com>
2023-03-19 01:05:44 -0700

    add 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main
+ *
+ * return: zero
+ */
+int main(void) 
+{
+	printf("Size of a char: %d byte(s)\n", Sizeof(char));
+	printf("Size of an int: %d byte(s)\n" Sizeof(int));
+	printf("Size of a long int: %d byte(s)\n" Sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n" Sizeof(long long int));	
+	printf("Size of an float: %d byte(s)\n" Sizeof(float));
+
+	
+	return (0);
+}
