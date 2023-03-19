b184e6aa4e0ae19234b17340fee3aff37714c312
Awajis <ibotyle@gmail.com>
2023-03-19 01:39:45 -0700

    ad fix for 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  * main: program entry point.
  *
- * return: zero.
+ * return: 0
  *
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art, ");
 	return (0);
 }
