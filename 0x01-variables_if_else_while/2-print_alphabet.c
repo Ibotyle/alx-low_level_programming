4c767498956d0f439f98a7c6857da24b60e1696c
Awajis <ibotyle@gmail.com>
2023-03-19 09:52:56 -0700

    add 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point to program
+ *
+ * Return: return value always o
+ *
+ */
+int main(void)
+{
+	for (int x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	putchar ('\n');
+	return (0);
+}
