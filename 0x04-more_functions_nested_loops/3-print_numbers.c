3ed3a13f18bd097a3a6b302ae0e467dc84a4a22c
Awajis <ibotyle@gmail.com>
2023-03-23 08:42:31 -0700

    add 5 more numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
- * print_numbers - prints the numbers
- *
+ * print_numbers - prints numbers from 0 to 9, followed by a new line
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 48; i < 58; i++)
 	{
 		_putchar(i);
 	}
