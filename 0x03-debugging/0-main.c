0d9f9d2c4b0ce0474413683fc38ad2e482f3d91e
Awajis <ibotyle@gmail.com>
2023-03-23 07:36:33 -0700

    add initial commit for debugging task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * positive_or_negative - check the function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        /* your code goes there */
+        if (n < 0)
+        {
+                printf("%d is negative\n", n);
+        }
+        if (n == 0)
+        {
+                printf("%d is zero\n", n);
+        }
+        if (n > 0)
+        {
+                printf("%d is positive\n", n);
+        }
+        return (0);
