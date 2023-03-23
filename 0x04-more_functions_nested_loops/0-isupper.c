ed45308c0a514001c114031006a99db8083211cf
Awajis <ibotyle@gmail.com>
2023-03-23 08:32:38 -0700

    add print most numbers

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,15 @@
+#include "main.h"
 /**
- * _isupper - access point to code
+ * _isupper - checks if uppercase
  * @c: character to check
- * 
- * Return: (1) if uppercase, (0) if not
+ *
+ * Return: gives 1 or 0
  */
-int _isupper(int c);
+int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
+	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 	return (0);
 }
